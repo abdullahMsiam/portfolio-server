c3ce91bfec14526d2399d7087eabcef732135fe4
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2025-01-06 17:11:59 +0600

    connect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
@@ -9,7 +10,6 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8l0xr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
+const run = async () => {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
@@ -146,7 +146,7 @@ async function run() {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
-}
+};
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
